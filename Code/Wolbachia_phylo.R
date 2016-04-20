3256fabb0aaef6b86be017662dc3247106add069
Chris Hamm <chamm@ku.edu>
2016-04-20 12:48:53 -0400

    removed duplicates from tree so the vcv matrix is clean

--- Code/Wolbachia_phylo.R
+++ Code/Wolbachia_phylo.R
@@ -14,8 +14,17 @@ Lep.tree <- read.nexus("Regier_data/Fam_codes.TRE")
 Lep.tree
 head(Lep.tree$tip.label)
 
-plot.phylo(Lep.tree, cex = 0.4, no.margin = TRUE)
+length(Lep.tree$tip.label)
+length(unique(Lep.tree$tip.label))
 
+duplicated(Lep.tree$tip.label)
+Lep.tree$tip.label[duplicated(Lep.tree$tip.label)]
+get.off <- which(duplicated(Lep.tree$tip.label))
 
-Lep.vcv <- vcvPhylo(tree = Lep.tree, anc.nodes = FALSE, model = "BM")
+Lep.nodups <- drop.tip(Lep.tree, tip = get.off)
+length(Lep.nodups$tip.label)
+plot.phylo(Lep.nodups, cex = 0.4, no.margin = TRUE)
+duplicated(Lep.nodups$tip.label)
+
+Lep.vcv <- vcvPhylo(tree = Lep.nodups, anc.nodes = FALSE, model = "BM")
 head(Lep.vcv)
