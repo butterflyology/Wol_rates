3e2376c8161885d5aa97e6f7f6cbd4655cdb6b74
Zachary Marion <zmarion@utk.edu>
2016-04-26 11:01:51 -0400

    Updated processing of phylo vcv.

--- Code/Wolbachia_phylo.R
+++ Code/Wolbachia_phylo.R
@@ -15,10 +15,10 @@ Lep.tree
 head(Lep.tree$tip.label)
 
 
-plot.phylo(Lep.tree, cex = 0.4, no.margin = TRUE)
+#plot.phylo(Lep.tree, cex = 0.4, no.margin = TRUE)
 
 
-Lep.vcv <- vcvPhylo(tree = Lep.tree, anc.nodes = FALSE, model = "BM")
+#Lep.vcv <- vcvPhylo(tree = Lep.tree, anc.nodes = FALSE, model = "BM")
 
 length(Lep.tree$tip.label)
 length(unique(Lep.tree$tip.label))
@@ -35,26 +35,23 @@ plot.phylo(Lep.nodups, cex = 0.5, no.margin = TRUE)
 # dev.off()
 duplicated(Lep.nodups$tip.label)
 
-Lep.vcv <- vcv.phylo(phy = Lep.nodups, corr = TRUE, model = "BM")
-oPhy <- order(colnames(Lep.vcv))
-Lep.vcv <- Lep.vcv[oPhy, oPhy]
-
-x <- famCode %in% colnames(Lep.vcv) != TRUE
-famCode[x]
-
+###### Get Weinert data ready
 weinDat <- read.csv("Data/Weinert_data_cleaned.csv", stringsAsFactors = FALSE)
 
-wol <- weinDat[weinDat$Bacterium == "Wolbachia" & weinDat$Order == "lepidoptera" & weinDat$Infected <= weinDat$Total & weinDat$Family != "Unid." & weinDat$Family != "Unid",]
+wol <- weinDat[weinDat$Bacterium == "Wolbachia" & weinDat$Order == "lepidoptera" & weinDat$Infected <= weinDat$Total & weinDat$Family != "Unid." & weinDat$Family != "Unid" & weinDat$Family != "Riodinidae",]
 
 wol$spp <- paste(wol$Family,wol$species, sep = "_")
 wol$fam <- wol$Family
 wol <- wol[order(wol$spp),]
 famCode <- casefold(substring(unique(wol$fam), 1,4), upper=TRUE)
-names(wol)
 
+unShared <- which(Lep.nodups$tip.label %in% famCode == FALSE)
 
+finalTree <- drop.tip(Lep.nodups, tip = unShared)
+save(finalTree, file="finalTree.R")
 
+Lep.vcv <- vcv.phylo(phy = finalTree, corr = TRUE, model = "BM")
+oPhy <- order(colnames(Lep.vcv))
+Lep.vcv <- Lep.vcv[oPhy, oPhy]
 
-
-
-head(Lep.vcv)
+save(Lep.vcv, file="Lep.vcv.R")
