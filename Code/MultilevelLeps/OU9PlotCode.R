39cc2cb6088ec3ebd548a22308ed1e61236d92bc
Zachary Marion <zmarion@utk.edu>
2016-05-14 11:45:16 -0600

    tweaked plots

--- Code/MultilevelLeps/OU9PlotCode.R
+++ Code/MultilevelLeps/OU9PlotCode.R
@@ -7,13 +7,51 @@ setwd("~/Dropbox/Wol_rates")
 # setwd("~/Desktop/Projects/Wolbachia_rates")
 source("Code/pplots.R")
 load("~/Dropbox/Wol-Leps/MultiLevModel/stanMods.R")
+load("Data/Lep.vcv.ultra.R")
+# source("~/Dropbox/Wol-Leps/MultiLevModel/stanMods.R")
+
+weinDat <- read.csv("Data/Weinert_data_cleaned.csv", stringsAsFactors = FALSE)
+
+
+
+wol <- weinDat[weinDat$Bacterium == "Wolbachia" 
+               & weinDat$Order == "lepidoptera" 
+               & weinDat$Infected <= weinDat$Total
+               & weinDat$Family != "Unid." 
+               & weinDat$Family != "Unid" 
+               & weinDat$Family != "Riodinidae", ]
+
+wol$spp <- paste(wol$Family, wol$species, sep = "_")
+wol$fam <- wol$Family
+wol <- wol[order(wol$spp), ]
+str(wol)
+sum(wol$Total)
+sum(wol$Infected)
+unique(wol$spp)
+length(unique(wol$spp))
+length(unique(wol$Family))
+
+dim(wol[wol$Infected >= 0, ])
+dim(wol[wol$Infected >= 1, ])
+dim(wol[wol$Infected == 0, ])
+
+pos <- wol[wol$Infected >= 1, ]
+length(unique(pos$spp))
+neg <- wol[wol$Infected == 0, ]
+length(unique(neg$spp))
+tot <-wol[wol$Infected >= 0, ]
+length(unique(tot$Family))
+
+temp <- unique.matrix(cbind(wol$fam, wol$spp)) 
+
+
+
 
 fitOU9 <- mods$fitOU9
 
-
 # nPhy <- as.data.frame(fitNPhy, "infectF")
-# BM   <- as.data.frame(fitBM, "infectF")
-OU9  <- as.data.frame(fitOU9, "infectF")
+OU9   <- as.data.frame(fitOU5, "infectF")
+# OU1  <- as.data.frame(fitOU1, "infectF")
 # OU5  <- as.data.frame(fitOU5, "infectF")
 # OU9  <- as.data.frame(fitOU9, "infectF")
 
@@ -31,12 +69,17 @@ xlabel <- rep(" ", ncol(OU9))
 
 
 # quartz(width=7.09, height=6, bg="white")
+
+# pdf(file = "Images/Fam_freqs.pdf", bg = "white")
+
+
+
 par(mar = c(7, 3.4, 0.17, 0))
-pPlot2(prob ~ families, data = out, ylim = c(0, 1), line.fun = median, pal = "ghostbusters", bar.o = 0, line.o = 0.7, bean.o = 0.8, point.o = 0.05, yaxt = "l", bty = "l", las = 1, xaxt = "n", xlab = "", ylab="", point.pch=16)
+pPlot2(prob ~ families, data = out, ylim = c(0, 1), line.fun = median, pal = "ghostbusters", bar.o = 0, line.o = 0.7, bean.o = 0.8, point.o = 0.05, yaxt = "l", bty = "l", las = 1, xaxt = "n", xlab = "", ylab="", point.pch=19)
 
-pPlot3(prob ~ families, data = out, ylim = c(0, 1), line.fun = lower, pal = "ghostbusters", bar.o = 0, line.o = 0.7, bean.o = 0.0, point.o = 0, yaxt = "l", bty = "l", las = 1, xaxt = "n", xlab = "", ylab="", line.lwd=2.5, add=TRUE)
+pPlot3(prob ~ families, data = out, ylim = c(0, 1), line.fun = lower, pal = "ghostbusters", bar.o = 0, line.o = 0.7, bean.o = 0.0, point.o = 0, yaxt = "l", bty = "l", las = 1, xaxt = "n", xlab = "", ylab="", line.lwd=2.5, add=TRUE, point.pch=19)
 
-pPlot3(prob ~ families, data = out, ylim = c(0, 1), line.fun = upper, pal = "ghostbusters", bar.o = 0, line.o = 0.7, bean.o = 0, point.o = 0, yaxt = "l", bty = "l", las = 1, xaxt = "n", xlab = "", ylab="", line.lwd=2.5, add=TRUE)
+pPlot3(prob ~ families, data = out, ylim = c(0, 1), line.fun = upper, pal = "ghostbusters", bar.o = 0, line.o = 0.7, bean.o = 0, point.o = 0, yaxt = "l", bty = "l", las = 1, xaxt = "n", xlab = "", ylab="", line.lwd=2.5, add=TRUE, point.pch=19)
 
 
 mtext("Infection probability", 2, cex=1.11, line = 2.37, font=1)
